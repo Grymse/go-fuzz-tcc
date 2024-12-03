abf36426f0b5521749e5408fe3cfd96e6537696d
Faurby <lasse_fk@hotmail.com>
2024-12-03 11:27:59 +0100

    chore: even smaller fix. And add 1.1 comparison

--- coverage/scripts/run_all_plots.sh
+++ coverage/scripts/run_all_plots.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
 python $script_dir/plot_coverage_lines_executed.py
 python $script_dir/plot_coverage_branches_executed.py
 python $script_dir/plot_coverage_calls_executed.py
